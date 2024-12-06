08491e09592a9288c35492311aa017def83baa77
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-06 15:46:46 -0500

    Remove this.khipu from Hero

--- js/hero.js
+++ js/hero.js
@@ -1,15 +1,15 @@
 function Hero(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'hero');
-    this.khipu.anchor.set(0.5, 0.5);
+    this.anchor.set(0.5, 0.5);
 
     // physic properties
-    this.khipu.game.physics.enable(this);
-    this.khipu.body.collideWorldBounds = true;
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
 
-    this.khipu.animations.add('stop', [0]);
-    this.khipu.animations.add('run', [1, 2], 8, true); // 8fps looped
-    this.khipu.animations.add('jump', [3]);
-    this.khipu.animations.add('fall', [4]);
+    this.animations.add('stop', [0]);
+    this.animations.add('run', [1, 2], 8, true); // 8fps looped
+    this.animations.add('jump', [3]);
+    this.animations.add('fall', [4]);
 
 }
 
@@ -19,23 +19,23 @@ Hero.prototype.constructor = Hero;
 
 Hero.prototype.move = function (direction) {
     const SPEED = 200;
-    this.khipu.body.velocity.x = direction * SPEED;
+    this.body.velocity.x = direction * SPEED;
 
     // update image flipping & animations
-    if (this.khipu.body.velocity.x < 0) {
-        this.khipu.scale.x = -1;
+    if (this.body.velocity.x < 0) {
+        this.scale.x = -1;
     }
-    else if (this.khipu.body.velocity.x > 0) {
-        this.khipu.scale.x = 1;
+    else if (this.body.velocity.x > 0) {
+        this.scale.x = 1;
     }
 };
 
 Hero.prototype.jump = function () {
     const JUMP_SPEED = 600;
-    let canJump = this.khipu.body.touching.down;
+    let canJump = this.body.touching.down;
 
     if (canJump) {
-        this.khipu.body.velocity.y = -JUMP_SPEED;
+        this.body.velocity.y = -JUMP_SPEED;
     }
 
     return canJump;
@@ -43,14 +43,14 @@ Hero.prototype.jump = function () {
 
 Hero.prototype.bounce = function () {
     const BOUNCE_SPEED = 200;
-    this.khipu.body.velocity.y = -BOUNCE_SPEED;
+    this.body.velocity.y = -BOUNCE_SPEED;
 };
 
 Hero.prototype.update = function () {
     // update sprite animation, if it needs changing
-    let animationName = this.khipu._getAnimationName();
-    if (this.khipu.animations.name !== animationName) {
-        this.khipu.animations.play(animationName);
+    let animationName = this._getAnimationName();
+    if (this.animations.name !== animationName) {
+        this.animations.play(animationName);
     }
 };
 
@@ -58,14 +58,14 @@ Hero.prototype._getAnimationName = function () {
     let name = 'stop'; // default animation
 
     // jumping
-    if (this.khipu.body.velocity.y < 0) {
+    if (this.body.velocity.y < 0) {
         name = 'jump';
     }
     // falling
-    else if (this.khipu.body.velocity.y >= 0 && !this.khipu.body.touching.down) {
+    else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
         name = 'fall';
     }
-    else if (this.khipu.body.velocity.x !== 0 && this.khipu.body.touching.down) {
+    else if (this.body.velocity.x !== 0 && this.body.touching.down) {
         name = 'run';
     }
 
