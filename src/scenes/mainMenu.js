1ec74cfe17fd830b6c8316e3041b4f227f3e931f
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-07 18:56:19 -0500

    Create MainMenu scene to construction of Hero (platforms incorrect)

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -1,39 +1,35 @@
+import Phaser from "phaser";
+import Hero from "../sprites/hero";
 export default class MainMenu extends Phaser.Scene {
     constructor() {
         super({ key: "MainMenu" });
     }
     init(data) {
-        this.game.renderer.renderSession.roundPixels = true;
-    
-        this.keys = this.game.input.keyboard.addKeys({
-            left: Phaser.KeyCode.LEFT,
-            right: Phaser.KeyCode.RIGHT,
-            up: Phaser.KeyCode.UP
+        this.keys = this.input.keyboard.addKeys({
+            left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+            right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+            up: Phaser.Input.Keyboard.KeyCodes.UP
         });
-    
-        this.keys.up.onDown.add(function () {
-            let didJump = this.hero.jump();
-            if (didJump) {
-                this.sfx.jump.play();
-            }
-        }, this);
     }
     
     preload() {
-        this.game.load.json('start0', 'data/start0.json');
-        this.game.load.image('startgame', 'images/startgame.jpg');
-        this.game.load.image('ground', 'images/ground.png');
+        this.load.json('start0', 'data/start0.json');
+        this.load.image('startgame', 'assets/images/startgame.jpg');
+        this.load.image('ground', 'assets/images/ground.png');
     
-        this.game.load.image('button', 'images/websitelogo1.png');
-        this.game.load.image('grass:6x1', 'images/starttile.png');
+        this.load.image('button', 'assets/images/websitelogo1.png');
+        this.load.image('grass:6x1', 'assets/images/starttile.png');
         
-        this.game.load.image('button2', 'images/instagramlogo.png');
-        this.game.load.image('button3', 'images/facebooklogo.png');
+        this.load.image('button2', 'assets/images/instagramlogo.png');
+        this.load.image('button3', 'assets/images/facebooklogo.png');
         
-        this.game.load.spritesheet('hero', 'images/hero.png', 36, 42);
+        this.load.spritesheet('hero', 'assets/images/hero.png', {
+            frameWidth: 36,
+            frameHeight: 42,
+          });
         
-        this.game.load.audio('sfx:jump', 'audio/jump.wav');
-        this.game.load.audio('sfx:platform', 'audio/coin.wav');
+        this.load.audio('sfx:jump', 'assets/audio/jump.wav');
+        this.load.audio('sfx:platform', 'assets/audio/coin.wav');
     }
     
     _openWindow() {
@@ -49,19 +45,19 @@ export default class MainMenu extends Phaser.Scene {
     }
 
     create() {
-            // create sound entities
+        // create sound entities
         this.sfx = {
-            jump: this.game.add.audio('sfx:jump'),
-            platform: this.game.add.audio('sfx:platform'),
+            jump: this.sound.add('sfx:jump'),
+            platform: this.sound.add('sfx:platform'),
         };
         // create level
-        this.game.add.image(0, 0, 'startgame');
-        this._loadLevel(this.game.cache.getJSON('start0'));
+        this.add.image(0, 0, 'startgame');
+        this._loadLevel(this.cache.json.get('start0'));
     
-        const button = this.game.add.button(855, 26, 'button', this._openWindow, this);
+        const button = this.add.button(855, 26, 'button', this._openWindow, this);
         button.input.useHandCursor = true;
-        this.game.add.button(855, 126, 'button2', this._openInsta, this);
-        this.game.add.button(855, 226, 'button3', this._openFace, this);
+        this.add.button(855, 126, 'button2', this._openInsta, this);
+        this.add.button(855, 226, 'button3', this._openFace, this);
     }
 
     update() {
@@ -70,12 +66,18 @@ export default class MainMenu extends Phaser.Scene {
     }
     
     _handleCollisions() {
-        this.game.physics.arcade.collide(this.hero, this.platforms);
-        this.game.physics.arcade.overlap(this.hero, this.coin, this._onHeroVsCoin, null, this);
+        this.physics.arcade.collide(this.hero, this.platforms);
+        this.physics.arcade.overlap(this.hero, this.coin, this._onHeroVsCoin, null, this);
     };
     
     _handleInput() {
-        if (this.keys.left.isDown) { // move hero left
+        if (Phaser.Input.Keyboard.JustDown(this.keys.up)) {
+            let didJump = this.hero.jump();
+            if (didJump) {
+                this.sfx.jump.play();
+            }
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.keys.left)) { // move hero left
             this.hero.move(-1);
         } else if (this.keys.right.isDown) { // move hero right
             this.hero.move(1);
@@ -86,47 +88,33 @@ export default class MainMenu extends Phaser.Scene {
 
     _loadLevel(data) {
         // create all the groups/layers that we need
-        this.platforms = this.game.add.group();
-        this.coin = this.game.add.group();
+        this.platforms = this.add.group();
+        this.coin = this.add.group();
         // spawn all platforms
         data.platforms.forEach(this._spawnPlatform, this);
         data.coin.forEach(this._spawnCoin, this);
         // spawn hero and enemies
         this._spawnCharacters({hero: data.hero});
-        // enable gravity
-        const GRAVITY = 1200;
-        this.game.physics.arcade.gravity.y = GRAVITY;
     }
     
     _spawnPlatform(platform) {
-        let sprite = this.platforms.create(
-            platform.x, platform.y, platform.image);
-    
-        this.game.physics.enable(sprite);
-        sprite.body.allowGravity = false;
-        sprite.body.immovable = true;
+        this.platforms.create(platform.x, platform.y, platform.image);
     }
     
     _spawnCoin(coin) {
-        let sprite = this.coin.create(coin.x, coin.y, coin.image);
-    
-        this.game.physics.enable(sprite);
-        sprite.body.allowGravity = false;
-        sprite.body.immovable = true;
+        this.coin.create(coin.x, coin.y, coin.image);
     }
     
     _spawnCharacters(data) {
-        // spawn hero
-        this.hero = new Hero(this.game, data.hero.x, data.hero.y);
-        this.game.add.existing(this.hero);
+        this.hero = new Hero(this, data.hero.x, data.hero.y);
     }
     
     _onHeroVsCoin(_hero, platform) {
         this.sfx.platform.play();
         platform.kill();
         
-        this.game.state.add('play', PlayState);
-            this.game.state.start('play', true, false, {level: 0});
+        this.game.scene('play', PlayState);
+        this.game.scene.start('play', true, false, {level: 0});
     }
     
     shutdown() {}
