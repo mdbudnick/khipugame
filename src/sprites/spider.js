55671dc2164c3e4e71b99a919a4415468957d182
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-07 14:03:53 -0500

    Create class Spider extends Phaser.GameObjects.Sprite

--- src/sprites/spider.js
+++ src/sprites/spider.js
@@ -1,39 +1,31 @@
-function Spider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'spider');
+class Spider extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, "spider")
+        this.anchor.set(0.5);
 
-    // anchor
-    this.anchor.set(0.5);
-    // animation
-    this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
-    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
-    this.animations.play('crawl');
+        this.game.physics.enable(this);
+        this.body.collideWorldBounds = true;
+        this.body.velocity.x = 100;
 
-    // physic properties
-    this.game.physics.enable(this);
-    this.body.collideWorldBounds = true;
-    this.body.velocity.x = Spider.SPEED;
+        this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
+        this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+        this.animations.play('crawl');
+    }
+
+    update() {
+        // check against walls and reverse direction if necessary
+        if (this.body.touching.right || this.body.blocked.right) {
+            this.body.velocity.x = -Spider.SPEED; // turn left
+        } else if (this.body.touching.left || this.body.blocked.left) {
+            this.body.velocity.x = Spider.SPEED; // turn right
+        }
+    }
+    
+    die() {
+        this.body.enable = false;
+    
+        this.animations.play('die').onComplete.addOnce(function () {
+            this.kill();
+        }, this);
+    }
 }
-
-Spider.SPEED = 100;
-
-// inherit from Phaser.Sprite
-Spider.prototype = Object.create(Phaser.Sprite.prototype);
-Spider.prototype.constructor = Spider;
-
-Spider.prototype.update = function () {
-    // check against walls and reverse direction if necessary
-    if (this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -Spider.SPEED; // turn left
-    }
-    else if (this.body.touching.left || this.body.blocked.left) {
-        this.body.velocity.x = Spider.SPEED; // turn right
-    }
-};
-
-Spider.prototype.die = function () {
-    this.body.enable = false;
-
-    this.animations.play('die').onComplete.addOnce(function () {
-        this.kill();
-    }, this);
-};
