6df426ba310366275d87035be174f15703393ef1
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-07 20:13:22 -0500

    Export sprites as default

--- src/sprites/spider.js
+++ src/sprites/spider.js
@@ -1,4 +1,4 @@
-export class Spider extends Phaser.GameObjects.Sprite {
+export default class Spider extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, "spider")
         this.anchor.set(0.5);
