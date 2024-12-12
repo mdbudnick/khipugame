6e9cb27900aa016b9e0226e0375c7315acfc4705
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-11 21:22:40 -0500

    Add back collideWorldBounds

--- src/sprites/hero.js
+++ src/sprites/hero.js
@@ -1,8 +1,9 @@
 export default class Hero extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
-        super(scene, x, y, "hero")
+        super(scene, x, y, 'hero')
         scene.add.existing(this);
         scene.physics.add.existing(this);
+        this.body.collideWorldBounds = true;
         this.body.setAllowGravity(true);
         this.setOrigin(0.5, 0.5);
         this.body.setCollideWorldBounds = true;
