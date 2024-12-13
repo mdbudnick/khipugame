3e104fa2a2e0aeec1c7115bb67dbd43ef5fa450b
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-12-13 10:13:11 -0500

    Background loading for play scene

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -1,87 +1,102 @@
 import Hero from "../sprites/hero";
 import Spider from "../sprites/spider";
 export default class Play extends Phaser.Scene {
-    LEVEL_COUNT = 4;
 
     constructor() {
         super({ key: "play" });
+        this.LEVEL_COUNT = 4;
     }
 
     init(data) {
-        this.keys = this.game.input.keyboard.addKeys({
-            left: Phaser.KeyCode.LEFT,
-            right: Phaser.KeyCode.RIGHT,
-            up: Phaser.KeyCode.UP
+        this.keys = this.input.keyboard.addKeys({
+            left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+            right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+            up: Phaser.Input.Keyboard.KeyCodes.UP
         });
         
         this.coinPickupCount = 0;
     
-        this.keys.up.onDown.add(function () {
-            let didJump = this.hero.jump();
-            if (didJump) {
-                this.sfx.jump.play();
-            }
-        }, this);
-    
         this.hasKey = 0;
-        this.level = (data.level || 0) % LEVEL_COUNT;
+        this.level = (data.level || 0) % this.LEVEL_COUNT;
     }
     
     preload() {
-        this.game.load.json('level:0', 'data/level00.json');
-        this.game.load.json('level:1', 'data/level01.json');
-        this.game.load.json('level:2', 'data/level02.json');
-        this.game.load.json('level:3', 'data/level03.json');
+        this.load.json('level:0', 'assets/data/level00.json');
+        this.load.json('level:1', 'assets/data/level01.json');
+        this.load.json('level:2', 'assets/data/level02.json');
+        this.load.json('level:3', 'assets/data/level03.json');
     
-        this.game.load.image('font:numbers', 'assets/images/numbers.png');
+        this.load.image('font:numbers', 'assets/images/numbers.png');
     
-        this.game.load.image('background', 'assets/images/background.png');
+        this.load.image('background', 'assets/images/background.png');
         
-        this.game.load.image('startgame', 'assets/images/startgame.png');
-        this.game.load.image('endgame', 'assets/images/endgame.png');
+        this.load.image('startgame', 'assets/images/startgame.png');
+        this.load.image('endgame', 'assets/images/endgame.jpg');
         
-        this.game.load.image('ground', 'assets/images/ground.png');
-        this.game.load.image('grass:8x1', 'assets/images/grass_8x1.png');
-        this.game.load.image('grass:6x1', 'assets/images/grass_6x1.png');
-        this.game.load.image('grass:4x1', 'assets/images/grass_4x1.png');
-        this.game.load.image('grass:2x1', 'assets/images/grass_2x1.png');
-        this.game.load.image('grass:1x1', 'assets/images/grass_1x1.png');
-        this.game.load.image('invisible-wall', 'assets/images/invisible_wall.png');
-        this.game.load.image('icon:coin', 'assets/images/coin_icon.png');
+        this.load.image('ground', 'assets/images/ground.png');
+        this.load.image('grass:8x1', 'assets/images/grass_8x1.png');
+        this.load.image('grass:6x1', 'assets/images/grass_6x1.png');
+        this.load.image('grass:4x1', 'assets/images/grass_4x1.png');
+        this.load.image('grass:2x1', 'assets/images/grass_2x1.png');
+        this.load.image('grass:1x1', 'assets/images/grass_1x1.png');
+        this.load.image('invisible-wall', 'assets/images/invisible_wall.png');
+        this.load.image('icon:coin', 'assets/images/coin_icon.png');
         
-        this.game.load.spritesheet('key', 'assets/images/key01.png', 60, 30);
-        this.game.load.spritesheet('badkey', 'assets/images/badkey01.png', 60, 30);
+        this.load.spritesheet('key', 'assets/images/key01.png', {
+            frameWidth: 60,
+            frameHeight: 30,
+          });
+        this.load.spritesheet('badkey', 'assets/images/badkey01.png',{
+            frameWidth: 60,
+            frameHeight: 30,
+          });
     
-        this.game.load.spritesheet('coin', 'assets/images/coin_animated.png', 22, 22);
-        this.game.load.spritesheet('spider', 'assets/images/spider.png', 43, 32);
-        this.game.load.spritesheet('hero', 'assets/images/hero.png', 36, 42);
-        this.game.load.spritesheet('door', 'assets/images/door.png', 42, 66);
-        this.game.load.spritesheet('icon:key', 'assets/images/key_icon.png', 34, 30);
+        this.load.spritesheet('coin', 'assets/images/coin_animated.png',{
+            frameWidth: 22,
+            frameHeight: 22,
+          });
+        this.load.spritesheet('spider', 'assets/images/spider.png',{
+            frameWidth: 43,
+            frameHeight: 32,
+          });
+        this.load.spritesheet('hero', 'assets/images/hero.png',{
+            frameWidth: 36,
+            frameHeight: 42,
+          });
+        this.load.spritesheet('door', 'assets/images/door.png',{
+            frameWidth: 42,
+            frameHeight: 66,
+          });
+        this.load.spritesheet('icon:key', 'assets/images/key_icon.png',{
+            frameWidth: 34,
+            frameHeight: 30,
+          });
     
-        this.game.load.audio('sfx:jump', 'assets/audio/jump.wav');
-        this.game.load.audio('sfx:coin', 'assets/audio/coin.wav');
-        this.game.load.audio('sfx:stomp', 'assets/audio/stomp.wav');
-        this.game.load.audio('sfx:key', 'assets/audio/key.wav');
-        this.game.load.audio('sfx:door', 'assets/audio/door.wav');
-        this.game.load.audio('sfx:bgm', ['assets/audio/bgm.mp3', 'assets/audio/bgm.ogg']);
+        this.load.audio('sfx:jump', 'assets/audio/jump.wav');
+        this.load.audio('sfx:coin', 'assets/audio/coin.wav');
+        this.load.audio('sfx:stomp', 'assets/audio/stomp.wav');
+        this.load.audio('sfx:key', 'assets/audio/key.wav');
+        this.load.audio('sfx:door', 'assets/audio/door.wav');
+        this.load.audio('sfx:bgm', ['assets/audio/bgm.mp3', 'assets/audio/bgm.ogg']);
     }
     
     create() {
         // create sound entities
         this.sfx = {
-            jump: this.game.add.audio('sfx:jump'),
-            coin: this.game.add.audio('sfx:coin'),
-            stomp: this.game.add.audio('sfx:stomp'),
-            key: this.game.add.audio('sfx:key'),
-            door: this.game.add.audio('sfx:door'),
-            bgm: this.game.add.audio('sfx:bgm')
+            jump: this.sound.add('sfx:jump'),
+            coin: this.sound.add('sfx:coin'),
+            stomp: this.sound.add('sfx:stomp'),
+            key: this.sound.add('sfx:key'),
+            door: this.sound.add('sfx:door'),
+            bgm: this.sound.add('sfx:bgm')
         };
-    this.sfx.bgm.loopFull();
+        this.sfx.bgm.setLoop(true);
+        this.sfx.bgm.play()
         // create level
-        this.game.add.image(0, 0, 'background');
-        this._loadLevel(this.game.cache.getJSON(`level:${this.level}`));
+        this.add.image(480, 300, 'background');
+        this._loadLevel(this.cache.json.get(`level:${this.level}`));
     
-        // crete hud with scoreboards)
+        // crete hud with scoreboards
         this._createHud();
     }
     
@@ -136,6 +151,12 @@ export default class Play extends Phaser.Scene {
     }
     
     _handleInput() {
+        if (Phaser.Input.Keyboard.JustDown(this.keys.up)) {
+            let didJump = this.hero.jump();
+            if (didJump) {
+                this.sfx.jump.play();
+            }
+        }
         if (this.keys.left.isDown) { // move hero left
             this.hero.move(-1);
         } else if (this.keys.right.isDown) { // move hero right
